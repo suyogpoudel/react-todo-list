2416a2c8aad94923f2e4daea5b054fbd2650ee62
Suyog Poudel <suyogpoudel583@gmail.com>
2025-06-16 18:50:05 +0545

    made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,12 @@ const App = () => {
             {(!todos || todos.length === 0)
                 ?
                 <p className='text-[25px] opacity-75 text-center mt-7'>No tasks added yet :(</p> :
-                <ul className='mt-7 w-[55%] mx-auto flex flex-col gap-2'>
+                <ul className='mt-7 w-[55%] max-sm:w-[90%] mx-auto flex flex-col gap-2'>
                     {todos.map(({task, done}, i) => (
                         <li key={i}>
                             <div className='flex justify-between items-center m-auto gap-2'>
-                                <p className={`${done ? 'line-through opacity-75 ' : ''} bg-slate-300 text-slate-900 py-2 px-4 text-[25px] rounded-lg w-[100%] cursor-pointer hover:opacity-90 active:opacity-75`} onClick={() => toggleDone(i)}>{task}</p>
-                                <button className='py-2 px-4 text-[25px] rounded-lg bg-red-900 cursor-pointer hover:opacity-90 active:opacity-75' onClick={() => deleteTask(i)}>Delete</button>
+                                <p className={`${done ? 'line-through opacity-75 ' : ''} bg-slate-300 text-slate-900 py-2 px-4 text-[25px] max-sm:text-[20px] rounded-l-lg rounded-r-none w-[100%] cursor-pointer hover:opacity-90 active:opacity-75`} onClick={() => toggleDone(i)}>{task}</p>
+                                <button className='py-2 px-4 text-[25px] max-sm:text-[20px] rounded-l-none rounded-r-lg bg-red-900 cursor-pointer hover:opacity-90 active:opacity-75' onClick={() => deleteTask(i)}>Delete</button>
                             </div>
                         </li>
                     ))}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = ({todos, setTodos, input, setInput}) => {
     return (
         <div className='mt-5 flex justify-center items-center'>
-            <form className='w-[55%] flex justify-between items-center gap-2'
+            <form className='w-[55%] flex justify-between items-center gap-2 max-sm:w-[90%]'
             onSubmit={e => {
                 e.preventDefault()
                 setTodos([...todos, {task: input, done: false}])
@@ -11,11 +11,11 @@ const Input = ({todos, setTodos, input, setInput}) => {
                 // console.log(todos);
             }}>
                 <input type="text"
-                className='w-[100%] py-2 px-4 text-[25px] rounded-l-lg border-[1.75px] border-slate-300 rounded-r-none'
+                className='w-[100%] py-2 px-4 text-[25px] max-sm:text-[20px] rounded-l-lg border-[1.75px] border-slate-300 rounded-r-none'
                 placeholder='Add new task'
                 value={input}
                 onChange={e => setInput(e.target.value)}/>
-                <input type="submit" value="Add Task" className='py-2 px-4 text-[25px] rounded-r-lg rounded-l-none bg-slate-300 border-[1.75px] border-slate-300 text-slate-900'/>
+                <input type="submit" value="Add Task" className='py-2 px-4 text-[25px] max-sm:text-[20px] rounded-r-lg rounded-l-none bg-slate-300 border-[1.75px] border-slate-300 text-slate-900'/>
             </form>
         </div>
     )
