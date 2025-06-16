ec9e8640f8dcf6a014410edc41e97494c1dec3e7
Suyog Poudel <suyogpoudel583@gmail.com>
2025-06-16 17:19:57 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react'
+import Input from "./components/Input.jsx";
+
+const App = () => {
+
+    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')))
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    })
+
+    const toggleDone = (index) => {
+        setTodos(todos.map((todo, i) =>
+            i === index ? { ...todo, done: !todo.done } : todo
+        ))
+    }
+
+    const deleteTask = (index) => {
+        setTodos(todos.filter((_,i) => i !== index))
+    }
+
+
+    return (
+        <div>
+            <h2 className='text-[45px] text-center'>To-Do List</h2>
+
+            <Input todos={todos} setTodos={setTodos} input={input} setInput={setInput}/>
+
+            {todos.length === 0 ?
+                <p className='text-[25px] opacity-75 text-center mt-7'>No tasks added yet :(</p> :
+                <ul className='mt-7 w-[55%] mx-auto flex flex-col gap-2'>
+                    {todos.map(({task, done}, i) => (
+                        <li key={i}>
+                            <div className='flex justify-between items-center m-auto gap-2'>
+                                <p className={`${done ? 'line-through opacity-75 ' : ''} bg-slate-300 text-slate-900 py-2 px-4 text-[25px] rounded-lg w-[100%] cursor-pointer hover:opacity-90 active:opacity-75`} onClick={() => toggleDone(i)}>{task}</p>
+                                <button className='py-2 px-4 text-[25px] rounded-lg bg-red-900 cursor-pointer hover:opacity-90 active:opacity-75' onClick={() => deleteTask(i)}>Delete</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            }
+        </div>
+
+
+    )
+}
+export default App
---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Input = ({todos, setTodos, input, setInput}) => {
+    return (
+        <div className='mt-5 flex justify-center items-center'>
+            <form className='w-[55%] flex justify-between items-center gap-2'
+            onSubmit={e => {
+                e.preventDefault()
+                setTodos([...todos, {task: input, done: false}])
+                setInput('');
+                // console.log(todos);
+            }}>
+                <input type="text"
+                className='w-[100%] py-2 px-4 text-[25px] rounded-l-lg border-[1.75px] border-slate-300 rounded-r-none'
+                placeholder='Add new task'
+                value={input}
+                onChange={e => setInput(e.target.value)}/>
+                <input type="submit" value="Add Task" className='py-2 px-4 text-[25px] rounded-r-lg rounded-l-none bg-slate-300 border-[1.75px] border-slate-300 text-slate-900'/>
+            </form>
+        </div>
+    )
+}
+export default Input
